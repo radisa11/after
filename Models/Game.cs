c49b33476a344823bfc9d9985f3a3412ad4e1a8b
radisa11 <zlatic66@.com>
2022-05-09 10:05:16 -0500

    first commit

---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+
+
+namespace after.Models
+{
+    public class Game
+    {
+        public string Name {get;set;}
+        public string Genre {get;set;}
+        public double Price {get;set;}
+    }
+}
